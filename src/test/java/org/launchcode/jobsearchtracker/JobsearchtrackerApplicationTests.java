f9c3941bba9162618e4e6be67a7ce74e437616fa
May Lin <40640630+maymlin@users.noreply.github.com>
2021-12-25 20:11:15 -0600

    first commit

---
+++ src/test/java/org/launchcode/jobsearchtracker/JobsearchtrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.jobsearchtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobsearchtrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
