d7ecc7858df9f61a2ca47c9e5c96e2d012055687
May Lin <40640630+maymlin@users.noreply.github.com>
2022-01-05 21:08:13 -0600

    https://spring.io/guides/tutorials/spring-boot-oauth2/

--- src/main/java/org/launchcode/jobsearchtracker/controllers/OAuthController.java
+++
@@ -1,4 +0,0 @@
-package org.launchcode.jobsearchtracker.controllers;
-
-public class OAuthController {
-}
---
+++ src/main/resources/static/script.js
@@ -0,0 +1,16 @@
+//    <script type="text/javascript">
+    $.get("/user", function(data) {
+        $("#user").html(data.name);
+        $(".unauthenticated").hide()
+        $(".authenticated").show()
+    });
+
+    var logout = function() {
+    $.post("/logout", function() {
+        $("#user").html('');
+        $(".unauthenticated").show();
+        $(".authenticated").hide();
+    })
+    return true;
+    }
+//    </script>
